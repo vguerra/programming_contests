f37744773711258dab6a24d0882388d305825500
Victor Guerra <vguerra@gmail.com>
2014-11-10 10:06:26 +0100

    Sorting strings and then iterate over the characters as if one would merge them (keeping them sorted ) and counting the characters that are equal between the two strings. Sustracting the count of repeated characters from the total amount of chars gives the numbers of chars that need to be deleted.

--- hackerrank/algos/sorting/make-it-anagram/make_anagram.cc
+++ hackerrank/algos/sorting/make-it-anagram/make_anagram.cc
@@ -8,13 +8,28 @@
 #include <algorithm>
 
 unsigned int count_extra_anagram(std::string s1, std::string s2) {
+  unsigned int counter = 0;
+  
   std::sort(s1.begin(), s1.end());
   std::sort(s2.begin(), s2.end());
 
-  unsigned int counter = 0;
-  return counter;
-}
+  auto it1 = s1.begin();
+  auto it2 = s2.begin();
 
+  while (it1 != s1.end() && it2 != s2.end()) {
+    if (*it1 < *it2) {
+      ++it1;
+    } else if (*it2 < *it1) {
+      ++it2;
+    } else {
+      counter += 2;
+      ++it1;
+      ++it2;
+    }
+  }
+
+  return s1.length() + s2.length() - counter;
+}
 
 int main() {
   std::string w1, w2;
